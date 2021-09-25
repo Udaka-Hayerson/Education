f5926f97820656446566cc311746b6a4a109b206
Udaka-Hayerson <mikehayerson@gmail.com>
2021-09-26 02:46:40 +0300

    решаю задачки на checkio.org

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-#from extending import SpaceObjects
+def left_join(phrases: tuple) -> str:
+    r = ''
+    phrases = list(phrases)
+    for i in phrases:
+        if 'right' in i:
+            r += i.replace('right', 'left')
+            r += ','
+        else:
+            r += (i + ',')
+    return r[:len(r)-1]
 
+
+if __name__ == "__main__":
+    print("Example:")
+    print(left_join(("left", "right", "left", "stop")))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert (
+        left_join(("left", "right", "left", "stop")) == "left,left,left,stop"
+    ), "All to left"
+    assert left_join(("bright aright", "ok")) == "bleft aleft,ok", "Bright Left"
+    assert left_join(("brightness wright",)) == "bleftness wleft", "One phrase"
+    assert left_join(("enough", "jokes")) == "enough,jokes", "Nothing to replace"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
